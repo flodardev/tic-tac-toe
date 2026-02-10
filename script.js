55a53485ecb9077c4a306b64d5ce55b32ad76936
flodardev <63950579+flodardev@users.noreply.github.com>
2026-02-10 15:55:02 +0800

    Add modules and factory functions
    gameBoard module done
    GameController module done
    createPlayer factory function done
    displayController to do next

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ const gameBoard = (() => {
   function placeMarker(index, playerMarker) {
     if (board[index] === "") {
       board[index] = playerMarker;
+      return true;
+    } else {
+      return false;
     }
   }
 
@@ -23,12 +26,104 @@ const gameBoard = (() => {
   };
 })();
 
+// The "brains"
+const gameController = (() => {
+  const playerOne = createPlayer("Player One", "X");
+  const playerTwo = createPlayer("Player Two", "O");
+
+  let activePlayer = playerOne;
+  let isGameOver = false;
+
+  const getActivePlayer = () => activePlayer;
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
+  };
+
+  const playRound = () => {
+    if (isGameOver) {
+      // game is over
+      return;
+    } else if (gameBoard.placeMarker(index, marker)) {
+      // check if its a winning move if not then switch players
+      if (winCheck()) {
+        // winner
+        isGameOver = true;
+      } else if (checkTie()) {
+        // its a tie
+        isGameOver = true;
+      } else {
+        switchPlayerTurn();
+      }
+    } else {
+      // re promt player to replace
+      console.log("That spot is taken! Please try again at another spot.");
+    }
+  };
+
+  const winCheck = () => {
+    let winner = false;
+
+    const winningRows = [
+      [0, 1, 2], // horizontal row
+      [3, 4, 5], // horizontal row
+      [6, 7, 8], // horizontal row
+      [0, 3, 6], // vertical row
+      [1, 4, 7], // vertical row
+      [2, 5, 8], // vertical row
+      [0, 4, 8], // diagonal row
+      [2, 4, 6], // diagonal row
+    ];
+
+    const currentBoard = gameBoard.getBoard();
+
+    winner = winningRows.some((row) => {
+      const values = [
+        currentBoard[row[0]],
+        currentBoard[row[1]],
+        currentBoard[row[2]],
+      ];
+
+      if (values.every((value) => value === values[0] && value !== "")) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    return winner;
+  };
+
+  const checkTie = () => {
+    const currentBoard = gameBoard.getBoard();
+    return currentBoard.every((item) => item !== "");
+  };
+
+  return {
+    getActivePlayer,
+    switchPlayerTurn,
+    playRound,
+    winCheck,
+  };
+})();
+
 // Display controller module
 const displayController = (() => {
   return {};
 })();
 
-// Facttory function for player
+// Factory function for player
 function createPlayer(name, marker) {
-  return { name, marker };
+  let score = 0;
+
+  const setScore = () => (score += 1);
+
+  const getScore = () => score;
+
+  return { name, marker, setScore, getScore };
 }
+
+gameBoard.placeMarker(0, "X");
+gameBoard.placeMarker(1, "X");
+gameBoard.placeMarker(2, "X");
+console.log(gameController.winCheck());
